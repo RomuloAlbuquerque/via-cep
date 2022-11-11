3b650d7ed1faa3f309a7c843691c5717fbf162bd
RomuloAlbuquerque <66214784+RomuloAlbuquerque@users.noreply.github.com>
2022-11-11 17:43:06 -0300

    name changed

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -7,7 +7,7 @@ const Home = () => {
     <div className="home-container">
       <Link to="/cepsearch">
         <div className="welcome">
-          <h2>Seja Bem Vindo ao App ViaCEP</h2>
+          <h2>Seja Bem Vindo ao App romulo.ViaCEP</h2>
         </div>
         <div className="button-container">
           <button className="btn btn-primary btn-lg start-button">
